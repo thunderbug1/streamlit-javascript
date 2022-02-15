506c785c9b58d1f956cff5626ae72e7f888c199d
bll <bll@tgw-group.com>
2022-02-15 10:55:03 +0100

    fixed MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="streamlit-javascript",
-    version="0.1.1",
+    version="0.1.2",
     author="Alexander Balasch",
     author_email="",
     description="component to run javascript code in streamlit application",
