0969044cc90aecba28353f7e744d1a630914060e
Alex <thunderbug1@users.noreply.github.com>
2023-06-19 08:45:31 +0200

    Update setup.py
    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="streamlit-javascript",
-    version="0.1.4",
+    version="0.1.5",
     author="Alexander Balasch",
     author_email="",
     description="component to run javascript code in streamlit application",
